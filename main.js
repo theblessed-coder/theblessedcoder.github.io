076c720313ea046360aa6877bdb88df3a4b5f54e
theblessed-coder <blessingnwakwuo8@gmail.com>
2023-11-14 17:05:04 +0100

    implemented media query that targets screens with a max width 768px

--- main.js
+++ main.js
@@ -89,10 +89,13 @@ function topFunction() {
     document.querySelector('.toggle-container').addEventListener('click', toggleDarkMode);
 
 //Mobile NavBar
+/* Open when someone clicks on the span element */
 function openNav() {
-  document.getElementById("myNav").style.height = "100%";
+  document.getElementById("myNav").style.width = "100%";
 }
 
+/* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
-  document.getElementById("myNav").style.height = "0%";
+  document.getElementById("myNav").style.width = "0%";
 }
+
