c753b88ab682a17d9dad018a9f3c7226fe8bbe7e
theblessed-coder <blessingnwakwuo8@gmail.com>
2023-12-14 14:48:14 +0100

    Update HTML and JS files with minor changes.

--- main.js
+++ main.js
@@ -29,12 +29,13 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
 }
 
-
 // Get the button
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -51,42 +52,44 @@ function topFunction() {
 }
 
 /* DarkMode */
-    const body = document.body;
-    const darkModeIcon = document.getElementById('dark-mode-icon');
-    const lightModeIcon = document.getElementById('light-mode-icon');
+const body = document.body;
+const darkModeIcon = document.getElementById("dark-mode-icon");
+const lightModeIcon = document.getElementById("light-mode-icon");
 
-    // Check user's preference from local storage
-    if (localStorage.getItem('dark-mode') === 'enabled') {
-        enableDarkMode();
-    }
+// Check user's preference from local storage
+if (localStorage.getItem("dark-mode") === "enabled") {
+  enableDarkMode();
+}
 
-    // Toggle between dark and light mode
-    function toggleDarkMode() {
-        if (body.classList.contains('dark-mode')) {
-            enableLightMode();
-        } else {
-            enableDarkMode();
-        }
-    }
+// Toggle between dark and light mode
+function toggleDarkMode() {
+  if (body.classList.contains("dark-mode")) {
+    enableLightMode();
+  } else {
+    enableDarkMode();
+  }
+}
 
-    // Enable dark mode
-    function enableDarkMode() {
-        body.classList.add('dark-mode');
-        localStorage.setItem('dark-mode', 'enabled');
-        darkModeIcon.style.display = 'none';
-        lightModeIcon.style.display = 'inline-block';
-    }
+// Enable dark mode
+function enableDarkMode() {
+  body.classList.add("dark-mode");
+  localStorage.setItem("dark-mode", "enabled");
+  darkModeIcon.style.display = "none";
+  lightModeIcon.style.display = "inline-block";
+}
 
-    // Enable light mode
-    function enableLightMode() {
-        body.classList.remove('dark-mode');
-        localStorage.setItem('dark-mode', null);
-        darkModeIcon.style.display = 'inline-block';
-        lightModeIcon.style.display = 'none';
-    }
+// Enable light mode
+function enableLightMode() {
+  body.classList.remove("dark-mode");
+  localStorage.setItem("dark-mode", null);
+  darkModeIcon.style.display = "inline-block";
+  lightModeIcon.style.display = "none";
+}
 
-    // Add event listener for the toggle container
-    document.querySelector('.toggle-container').addEventListener('click', toggleDarkMode);
+// Add event listener for the toggle container
+document
+  .querySelector(".toggle-container")
+  .addEventListener("click", toggleDarkMode);
 
 //Mobile NavBar
 /* Open when someone clicks on the span element */
@@ -98,4 +101,87 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
+
+// GSAP Animations
+
+const tl = gsap.timeline();
+
+gsap.to("h3", {
+  duration: 1,
+  ease: "back",
+});
+
+tl.fromTo(
+  "h1",
+  { scale: 0, opacity: 0 },
+  {
+    scale: 1,
+    ease: "back",
+    duration: 1,
+    opacity: 1,
+  }
+)
+  .fromTo(
+    "p",
+    { scale: 0, opacity: 0 },
+    {
+      scale: 1,
+      ease: "back",
+      duration: 1,
+      opacity: 1,
+      delay: 0.5,
+    }
+  )
+  .fromTo(
+    ".btn",
+    { scale: 0, opacity: 0 },
+    {
+      scale: 1,
+      ease: "back",
+      duration: 1,
+      opacity: 1,
+      delay: 0.5,
+    }
+  )
+  .fromTo(
+    ".contact",
+    { x: 100, opacity: 0 },
+    {
+      x: 0,
+      ease: "power2.inOut",
+      duration: 1,
+      opacity: 1,
+      delay: 0.5,
+    }
+  );
+
+//About section
+// Animate elements on scroll
+gsap.from(".about-content", {
+    opacity: 0,
+    y: 50,
+    duration: 0.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".about-content",
+        start: "top 70%",
+        end: "bottom 20%",
+        scrub: true,
+    }
+});
+
+gsap.from(".projects", {
+  opacity: 0,
+  y: 50,
+  duration: 0.5,
+  ease: "power2.out",
+  scrollTrigger: {
+    trigger: ".projects",
+    start: "top 70%",
+    end: "bottom 20%",
+    scrub: true,
+  },
+});
+
+
 
